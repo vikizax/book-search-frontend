fcaa0da779940f2ac606c19497141c764d91185e
Vivek <vivek.chetia@clinifyhealth.com>
2022-04-19 18:14:24 +0530

    beta:updated

---
+++ src/components/loginButton/index.tsx
@@ -0,0 +1,23 @@
+import { BrandGoogle } from "tabler-icons-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mantine/core";
+
+export default function LoginButton() {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <Button
+      variant="outline"
+      radius="md"
+      size="lg"
+      uppercase
+      leftIcon={<BrandGoogle size={20} strokeWidth={2} color={"#bf4040"} />}
+      onClick={() => {
+        loginWithRedirect({
+          redirectUri: "http://localhost:3000/search",
+        });
+      }}
+    >
+      Google Sign in
+    </Button>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { NotificationsProvider } from "@mantine/notifications";
+import { Auth0Provider } from "@auth0/auth0-react";
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+root.render(
+  <BrowserRouter>
+    <Auth0Provider
+      domain={domain!}
+      clientId={clientId!}
+      redirectUri={window.location.origin}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <NotificationsProvider>
+        <App />
+      </NotificationsProvider>
+    </Auth0Provider>
+  </BrowserRouter>
+);
